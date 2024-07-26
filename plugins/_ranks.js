bdbb00703e131f3abab86b3a2211a4ab27bb7794
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-26 17:19:59 +0300

    v5

--- plugins/_ranks.js
+++ plugins/_ranks.js
@@ -19,7 +19,7 @@ global.rpg = {
       { name: '🧝‍♂️ Master of Atlantis', level: 70 },
       { name: '🐲 Legendary Leviathan', level: 80 },
       { name: '🔮 Ocean Overlord 🔮', level: 90 },
-      { name: '🔱 Emperor of the Seven Seas 🔱', level: 100 },
+      { name: '🔱 Emperor of the Seven Seas with silva 🔱', level: 100 },
     ]
 
     return role.reverse().find(role => level >= role.level)
