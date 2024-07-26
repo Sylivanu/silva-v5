daf4386fc7075469ec7b234e5227235466d615d1
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-26 18:24:26 +0300

    v5

--- plugins/main-feature.js
+++ plugins/main-feature.js
@@ -6,14 +6,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = await conn.getName(who)
   let totalf = Object.values(global.plugins).filter(v => v.help && v.tags).length
-  let txt = `*乂  B O T  -  F E A T U R E*\n\n`
+  let txt = `*乂 『SILVA』『MD』 『V5 B O T  -  F E A T U R E*\n\n`
   txt += `	◦  *Total* : ${totalf}\n`
   txt += author
   await conn.relayMessage(
     m.chat,
     {
       requestPaymentMessage: {
-        currencyCodeIso4217: 'USD',
+        currencyCodeIso4217: 'KSH',
         amount1000: totalf * 1000,
         requestFrom: '0@s.whatsapp.net',
         noteMessage: {
